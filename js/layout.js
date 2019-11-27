5061fc13218efa4c5868d97f305c5509b981740c
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-27 22:40:00 +0100

    RegEx name

--- js/layout.js
+++ js/layout.js
@@ -8,6 +8,6 @@ if (UrlEncodedCode.length != 5 && UrlEncodedCode.length > 0) {
   console.log("Invalid code format");
 } else if (UrlEncodedCode == 0) {
   console.log("No code provided");
-} else if (!UrlEncodedCode.match(Exp)) {
+} else if (!UrlEncodedCode.match(alphaNumPatt)) {
   console.log("Code contains invalid characters");
 }
