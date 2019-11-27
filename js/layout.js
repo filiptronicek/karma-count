82f6042239cf32a1527deb36a5dec008770dc87e
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-27 22:39:13 +0100

    Code format checking

--- js/layout.js
+++ js/layout.js
@@ -2,9 +2,12 @@ const UrlEncodedCode = window.location.pathname.replace("/layout/", "");
 console.log(UrlEncodedCode);
 
 // Checks
+var alphaNumPatt = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
 
 if (UrlEncodedCode.length != 5 && UrlEncodedCode.length > 0) {
   console.log("Invalid code format");
 } else if (UrlEncodedCode == 0) {
   console.log("No code provided");
+} else if (!UrlEncodedCode.match(Exp)) {
+  console.log("Code contains invalid characters");
 }
