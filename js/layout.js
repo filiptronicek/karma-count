fdb866c9ce0ec4fc93d35c1f780f36cf03115649
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-27 22:33:51 +0100

    Checks

--- js/layout.js
+++ js/layout.js
@@ -1,2 +1,10 @@
-UrlEncodedCode = window.location.pathname.replace('/layout/', '');
-console.log(UrlEncodedCode)
+const UrlEncodedCode = window.location.pathname.replace("/layout/", "");
+console.log(UrlEncodedCode);
+
+// Checks
+
+if (UrlEncodedCode.length != 5 && UrlEncodedCode.length > 0) {
+  console.log("Invalid code format");
+} else if (UrlEncodedCode == 0) {
+  console.log("No code provided");
+}
