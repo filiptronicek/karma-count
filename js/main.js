eb497eca5d07372f3b4f7ebb662a58ab9843274f
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-27 19:36:18 +0100

    Format

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var leaderboard = document.getElementById('profiles');
-var settingsbtn = document.getElementById('settingsbtn');
-var plusbtn = document.getElementById('new');
+var leaderboard = document.getElementById("profiles");
+var settingsbtn = document.getElementById("settingsbtn");
+var plusbtn = document.getElementById("new");
 
 var commentKarma;
 var postKarma;
@@ -13,124 +13,127 @@ var newUserCounterEach = 0;
 var newUsr;
 
 usersById = [
-	'sloth_on_meth',
-	'filiptronicek',
-	'cigarkovic',
-	'gallowboob',
-	'tooshiftyforyou',
-	'haxpress',
-	'blokensie',
-	'ilovegaming123456',
-	'Trony55'
+  "sloth_on_meth",
+  "filiptronicek",
+  "cigarkovic",
+  "gallowboob",
+  "tooshiftyforyou",
+  "haxpress",
+  "blokensie",
+  "ilovegaming123456",
+  "Trony55"
 ];
 //Settings modal
 
-$('#checkbox').change(function (ev) {
-	if ($(this).is(':checked')) {
-		if (typeof Storage !== 'undefined') {
-			// Store
-			localStorage.setItem('reload', true);
-			console.log('Auto-reload changed to ' + localStorage.getItem('reload'));
+$("#checkbox").change(function(ev) {
+  if ($(this).is(":checked")) {
+    if (typeof Storage !== "undefined") {
+      // Store
+      localStorage.setItem("reload", true);
+      console.log("Auto-reload changed to " + localStorage.getItem("reload"));
 
-			// Retrieve
-		} else {}
-	} else {
-		if (typeof Storage !== 'undefined') {
-			// Store
-			localStorage.setItem('reload', false);
-			console.log('Auto-reload changed to ' + localStorage.getItem('reload'));
+      // Retrieve
+    } else {
+    }
+  } else {
+    if (typeof Storage !== "undefined") {
+      // Store
+      localStorage.setItem("reload", false);
+      console.log("Auto-reload changed to " + localStorage.getItem("reload"));
 
-			// Retrieve
-		}
-	}
+      // Retrieve
+    }
+  }
 
-	setTimeout(function () {
-		console.log('Shit just got updated');
-		location.href = '';
-	}, 100);
+  setTimeout(function() {
+    console.log("Shit just got updated");
+    location.href = "";
+  }, 100);
 });
-if (localStorage.getItem('reload') === 'true') {
-	console.log('Auto-reload enabled: ' + localStorage.getItem('reload'));
+if (localStorage.getItem("reload") === "true") {
+  console.log("Auto-reload enabled: " + localStorage.getItem("reload"));
 
-	$('#checkbox').attr('checked', 'true');
+  $("#checkbox").attr("checked", "true");
 
-	var Reload = function () {
-		console.log('Reloading in 5 seconds');
-		leaderboard.innerHTML += '<link rel="icon" href="img/down.png"></link>';
+  var Reload = function() {
+    console.log("Reloading in 5 seconds");
+    leaderboard.innerHTML += '<link rel="icon" href="img/down.png"></link>';
 
-		setTimeout(function () {
-			document.title = 'Loading';
-		}, 4500);
-		setTimeout(function () {
-			location.reload(true);
-		}, 5000);
-	};
-	setTimeout(function () {
-		Reload();
-	}, 55000);
+    setTimeout(function() {
+      document.title = "Loading";
+    }, 4500);
+    setTimeout(function() {
+      location.reload(true);
+    }, 5000);
+  };
+  setTimeout(function() {
+    Reload();
+  }, 55000);
 } else {
-	console.log('Auto-reload enabled: ' + localStorage.getItem('reload'));
+  console.log("Auto-reload enabled: " + localStorage.getItem("reload"));
 }
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = settingsbtn;
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function () {
-	modal.style.display = 'block';
+btn.onclick = function() {
+  modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-	modal.style.display = 'none';
+span.onclick = function() {
+  modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = 'none';
-	}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 };
 
 function mainfunc(user) {
-	$.getJSON('https://www.reddit.com/user/' + user + '/about.json', function (data) {
-			commentKarma = data.data.comment_karma;
-			postKarma = data.data.link_karma;
-			totalKarma = commentKarma + postKarma;
-			userName = user;
-			userIcon = data.data.icon_img;
-			userUrl = 'https://reddit.com/u/' + userName;
+  $.getJSON("https://www.reddit.com/user/" + user + "/about.json", function(
+    data
+  ) {
+    commentKarma = data.data.comment_karma;
+    postKarma = data.data.link_karma;
+    totalKarma = commentKarma + postKarma;
+    userName = user;
+    userIcon = data.data.icon_img;
+    userUrl = "https://reddit.com/u/" + userName;
 
-			usersloaded.push({
-				user,
-				userName,
-				userIcon,
-				userUrl,
-				totalKarma
-			});
+    usersloaded.push({
+      user,
+      userName,
+      userIcon,
+      userUrl,
+      totalKarma
+    });
 
-			loadData(usersloaded);
-		})
-		.done(function () {
-			return;
-		})
-		.fail(function () {
-			console.log('error loading ' + user);
-		})
-		.always(function () {
-			//console.log('completed loading ' + user);
-		});
+    loadData(usersloaded);
+  })
+    .done(function() {
+      return;
+    })
+    .fail(function() {
+      console.log("error loading " + user);
+    })
+    .always(function() {
+      //console.log('completed loading ' + user);
+    });
 }
 //Karma API
 
 function updateStats() {
-	usersById.forEach(mainfunc);
+  usersById.forEach(mainfunc);
 }
 /*
 function updaat{
@@ -142,74 +145,69 @@ function updaat{
 updateStats();
 
 function loadData(usersloaded) {
-	leaderboard.innerHTML = '';
-	usersloaded.sort((a, b) => b.totalKarma - a.totalKarma).forEach((u) => {
-		leaderboard.innerHTML +=
-			"<div class='usr' id='" +
-			u.userName +
-			"'><a href='" +
-			u.userUrl +
-			"'><br><br><img src='" +
-			u.userIcon +
-			"' height='256'><br> u/" +
-			u.userName +
-			'</a><br>' +
-			u.totalKarma.toLocaleString() +
-			' karma';
-	});
-	newUserCounterEach++;
-	console.log('Users: ' + newUserCounterEach);
+  leaderboard.innerHTML = "";
+  usersloaded
+    .sort((a, b) => b.totalKarma - a.totalKarma)
+    .forEach(u => {
+      leaderboard.innerHTML +=
+        "<div class='usr' id='" +
+        u.userName +
+        "'><a href='" +
+        u.userUrl +
+        "'><br><br><img src='" +
+        u.userIcon +
+        "' height='256'><br> u/" +
+        u.userName +
+        "</a><br>" +
+        u.totalKarma.toLocaleString() +
+        " karma";
+    });
+  newUserCounterEach++;
+  console.log("Users: " + newUserCounterEach);
 
-	document.title = 'Reddit karma';
-	leaderboard.innerHTML += '<link rel="icon" href="img/favicon.png"></link>';
+  document.title = "Reddit karma";
+  leaderboard.innerHTML += '<link rel="icon" href="img/favicon.png"></link>';
 }
 
 function addNew() {
-	console.log('Clicked plus btn');
-	newUsr = prompt('What is it?');
-	console.log(newUsr);
-	if (newUsr != undefined || newUsr != '') {
-		usersById.push(newUsr);
-		updateStats();
-	}
+  console.log("Clicked plus btn");
+  newUsr = prompt("What is it?");
+  console.log(newUsr);
+  if (newUsr != undefined || newUsr != "") {
+    usersById.push(newUsr);
+    updateStats();
+  }
 }
-$(document).ready(function () {
-	$('#github').hover(
-		function () {
-			$('#github').addClass('clockwise');
-		},
-		function () {
-			$('#github').removeClass('clockwise');
-		},
-		function () {
-			$('#github').removeClass('counterclockwise');
-		}
-	);
-});
 
-$('#github').hover(function () {
-	$(this).addClass('counterclockwise');
+$("#github").hover(function() {
+  $(this).addClass("counterclockwise");
 });
 var isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
-	}
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
 };
 if (isMobile.any()) {
-	$('#github').hide();
+  $("#github").hide();
 }
