7c57b1bfff9c932cf1efeb49444c4447ed9c8ff3
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-07 23:26:22 +0100

    Update inter.js

--- api/inter.js
+++ api/inter.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
 
-function sendRequest(code)
+function sendRequest(code) {
 axios.get(`http://unidev.hys.cz/includes/get-api?user=${code}`).then(res => {
     let url = res.data;
     return url;
     // send message to index.html
 });
+}
 
 module.exports = (req, res) => {
   const { code = "World" } = req.query;
