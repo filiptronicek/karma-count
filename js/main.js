560cd25261324c2e83e7af5b0d771aa379a2fa95
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-09 18:03:26 +0200

    Delete // @ts-ignore

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@
   DOM
 
 */
-// @ts-ignore
+
 const leaderboard = document.getElementById("profiles");
-// @ts-ignore
+
 const settingsbtn = document.getElementById("settingsbtn");
-// @ts-ignore
+
 const plusbtn = document.getElementById("new");
 
 // Get the modal
-// @ts-ignore
+
 const modal = document.getElementById("myModal");
 
 // Get the <span> element that closes the modal
-// @ts-ignore
+
 const span = document.getElementsByClassName("close")[0];
 
 let commentKarma;
@@ -23,21 +23,20 @@ let postKarma;
 let userName;
 let userIcon;
 let userUrl;
-// @ts-ignore
+
 let usersById;
-// @ts-ignore
+
 let usersloaded = [];
 let newUserCounterEach = 0;
 let newUsr;
 
 const date = new Date();
 const month = date.getMonth();
-if (month == 11 || month == 0 || month == 1) {
-  // @ts-ignore
+if (month === 11 || month === 0 || month === 1) {
   const sf = new Snowflakes({
     count: 80,
     maxSize: 20,
-    wind: false
+    wind: false,
   });
 }
 
@@ -51,7 +50,7 @@ usersById = [
   "blokensie",
   "ilovegaming123456",
   "Trony55",
-  "vexillologer"
+  "vexillologer",
 ];
 
 /*
@@ -59,98 +58,84 @@ usersById = [
   Settings modal
 
 */
-// @ts-ignore
-$("#checkbox").change(function(ev) {
-  // @ts-ignore
+
+$("#checkbox").change(function (ev) {
   if ($(this).is(":checked")) {
-    // @ts-ignore
     if (typeof Storage !== "undefined") {
       // Store
-      // @ts-ignore
+
       localStorage.setItem("reload", true);
-      // @ts-ignore
+
       console.log("Auto-reload changed to " + localStorage.getItem("reload"));
     }
   } else {
-    // @ts-ignore
     if (typeof Storage !== "undefined") {
       // Store
-      // @ts-ignore
+
       localStorage.setItem("reload", false);
-      // @ts-ignore
+
       console.log("Auto-reload changed to " + localStorage.getItem("reload"));
     }
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     console.log("Shit just got updated");
-    // @ts-ignore
+
     location.href = "";
   }, 100);
 });
-// @ts-ignore
+
 if (localStorage.getItem("reload") === "true") {
-  // @ts-ignore
   console.log("Auto-reload enabled: " + localStorage.getItem("reload"));
 
-  // @ts-ignore
   $("#checkbox").attr("checked", "true");
 
-  var Reload = function() {
+  var Reload = function () {
     console.log("Reloading in 5 seconds");
     leaderboard.innerHTML += '<link rel="icon" href="img/down.png"></link>';
 
-    setTimeout(function() {
-      // @ts-ignore
+    setTimeout(function () {
       document.title = "Loading";
     }, 4500);
-    setTimeout(function() {
-      // @ts-ignore
+    setTimeout(function () {
       location.reload(true);
     }, 5000);
   };
-  setTimeout(function() {
+  setTimeout(function () {
     Reload();
   }, 55000);
 } else {
-  // @ts-ignore
   console.log("Auto-reload enabled: " + localStorage.getItem("reload"));
 }
-
 
 // Get the button that opens the modal
 var btn = settingsbtn;
 
-
-
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
-// @ts-ignore
-window.onclick = function(event) {
+
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
 
-// @ts-ignore
 function mainfunc(user) {
-  // @ts-ignore
-  $.getJSON("https://www.reddit.com/user/" + user + "/about.json", function(
-    // @ts-ignore
+  $.getJSON("https://www.reddit.com/user/" + user + "/about.json", function (
     data
   ) {
     commentKarma = data.data.comment_karma;
     postKarma = data.data.link_karma;
-    // @ts-ignore
+
     totalKarma = commentKarma + postKarma;
     userName = user;
     userIcon = data.data.icon_img;
@@ -161,27 +146,24 @@ function mainfunc(user) {
       userName,
       userIcon,
       userUrl,
-      // @ts-ignore
-      totalKarma
+      totalKarma,
     });
 
-    // @ts-ignore
     loadData(usersloaded);
   })
-    .done(function() {
+    .done(function () {
       return;
     })
-    .fail(function() {
+    .fail(function () {
       console.log("error loading " + user);
     })
-    .always(function() {
+    .always(function () {
       //console.log('completed loading ' + user);
     });
 }
 //Karma API
 
 function updateStats() {
-  // @ts-ignore
   usersById.forEach(mainfunc);
 }
 /*
@@ -200,7 +182,7 @@ function loadData(usersloaded) {
   leaderboard.innerHTML = "";
   usersloaded
     .sort((a, b) => b.totalKarma - a.totalKarma)
-    .forEach(u => {
+    .forEach((u) => {
       leaderboard.innerHTML +=
         "<div class='usr' id='" +
         u.userName +
@@ -219,14 +201,12 @@ function loadData(usersloaded) {
   newUserCounterEach++;
   console.log("Users: " + newUserCounterEach);
 
-  // @ts-ignore
   document.title = "Reddit karma";
   leaderboard.innerHTML += '<link rel="icon" href="img/favicon.png"></link>';
 }
 
 function addNew() {
   console.log("Clicked plus btn");
-  // @ts-ignore
   newUsr = prompt("What is it?");
   console.log(newUsr);
   if (newUsr !== undefined || newUsr !== "") {
@@ -235,36 +215,22 @@ function addNew() {
   }
 }
 
-// @ts-ignore
-$("#github").hover(function() {
-  // @ts-ignore
+$("#github").hover(function () {
   $(this).addClass("counterclockwise");
 });
 const isMobile = {
-  Android: () =>
-    // @ts-ignore
-    navigator.userAgent.match(/Android/i),
-  BlackBerry: () =>
-    // @ts-ignore
-    navigator.userAgent.match(/BlackBerry/i),
-  iOS: () =>
-    // @ts-ignore
-    navigator.userAgent.match(/iPhone|iPad|iPod/i),
-  Opera: () =>
-    // @ts-ignore
-    navigator.userAgent.match(/Opera Mini/i),
-  Windows: () =>
-    // @ts-ignore
-    navigator.userAgent.match(/IEMobile/i),
-  any: () => (
+  Android: () => navigator.userAgent.match(/Android/i),
+  BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+  iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+  Opera: () => navigator.userAgent.match(/Opera Mini/i),
+  Windows: () => navigator.userAgent.match(/IEMobile/i),
+  any: () =>
     isMobile.Android() ||
     isMobile.BlackBerry() ||
     isMobile.iOS() ||
     isMobile.Opera() ||
-    isMobile.Windows()
-  )
+    isMobile.Windows(),
 };
 if (isMobile.any()) {
-  // @ts-ignore
   $("#github").hide();
 }
