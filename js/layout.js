e613667748837c25db460b354dfb5574e6b3a905
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-09 17:59:02 +0200

    Eqeqeq

--- js/layout.js
+++ js/layout.js
@@ -16,9 +16,9 @@ if (UrlEncodedCode == "layout_process.html") {
   location.href = window.location.origin;
 }
 
-if (UrlEncodedCode.length != 5 && UrlEncodedCode.length > 0) {
+if (UrlEncodedCode.length !== 5 && UrlEncodedCode.length > 0) {
   console.log("Invalid code format");
-} else if (UrlEncodedCode == 0) {
+} else if (UrlEncodedCode === 0) {
   console.log("No code provided");
 } else if (!UrlEncodedCode.match(alphaNumPatt)) {
   console.log("Code contains invalid characters");
